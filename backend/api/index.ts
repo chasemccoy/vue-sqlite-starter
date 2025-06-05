a75355438e8d9caa73c7b8c11d7d68c4a5dc401d
Chase McCoy <chasem000@gmail.com>
2025-06-04 21:43:31 -0500

    add readwise schema

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -5,5 +5,5 @@ const record = await getRecord(1);
 
 const tree = await getFamilyTree(1)
 
-// console.log(record);
+console.log(record);
 console.log(tree);
--- backend/db/schema/index.ts
+++ backend/db/schema/index.ts
@@ -1,2 +1,3 @@
 export * from './records';
 export * from './media';
+export * from './readwise';
